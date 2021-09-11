d8b7cc26710cf68cbcf84dd17b712b6fa248be1e
Dias2019 <dias_zhandar@mail.ru>
2021-09-12 03:26:12 +0800

    refactoring

---
+++ data.py
@@ -0,0 +1,30 @@
+
+
+
+
+# Data Preprocessing in the First Model:
+
+class data_first_model:
+
+    def __init__(self) -> None:
+        pass
+
+# Image Cropping:
+
+    def image_cropping(face_coordinates):
+        return 0
+
+# Image Normalization:
+
+    def image_normalization():
+        return 0
+
+# Down-sampling:
+
+    def down_sampling():
+        return 0
+
+# Data Augmentation:
+
+    def image_rotation():
+        return 0
